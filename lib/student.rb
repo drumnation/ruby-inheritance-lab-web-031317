c80a8fea807008b816c9fba0efe29f799eb446db
David Mieloch <davidmieloch@gmail.com>
2017-03-26 04:09:43 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User # Student inherits from User
+  attr_accessor :knowledge
+
+  def initialize()
+    @knowledge = []
+    # intitializes student with knowledge array
+  end
+
+  def learn(topic)
+    @knowledge << topic
+    # add random topics into the student object's knowledge array
+  end
 
 end
---
+++ lib/user.rb
@@ -0,0 +1,6 @@
+class User
+
+  attr_accessor :first_name, :last_name
+  # accessor to be inherited by student and teacher sub classes
+
+end
